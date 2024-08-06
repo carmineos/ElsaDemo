2410fe45333c41140b2d67e9dc7a3fa530622c71
carmineos <neos7@live.it>
2024-08-06 22:28:18 +0200

    add onboarding

---
+++ Onboarding/Data/OnboardingDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Onboarding.Entities;
+
+namespace Onboarding.Data;
+
+public class OnboardingDbContext(DbContextOptions<OnboardingDbContext> options) : DbContext(options)
+{
+    public DbSet<OnboardingTask> Tasks { get; set; } = default!;
+}
